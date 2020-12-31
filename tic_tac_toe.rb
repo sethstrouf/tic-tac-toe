4d2b8384b00ed8760e348058d0ff2105867e3a62
Seth Strouf <seth.strouf@doane.edu>
2020-12-31 12:01:10 -0600

    Cleaned up code using rubocop

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,53 +2,51 @@
 
 require 'pry'
 
-
 # Creates players/board and passes to a controller, tracks game behavior
 class Game
-  @@count_turns = 0
-
   def initialize(num_of_players, edge_size)
     @is_game_over = false
     @board = Board.new(edge_size)
     @controller = Controller.new(num_of_players)
+    @count_turns = 0
   end
 
-  #game loop: draw, get input, update board, check game over
-  #if @is_game_over then end_game
+  # game loop: draw, get input, update board, check game over
+  # if @is_game_over then end_game
   def start_game
     puts 'GAME STARTED'
-    while !@is_game_over
-      @@count_turns += 1
+    until @is_game_over
+      @count_turns += 1
       @board.draw_board
       @board.update_board(@controller.get_input)
-      is_game_over
+      game_over?
     end
-    puts @@count_turns
+    puts @count_turns
     end_game
   end
 
-  def is_game_over
+  def game_over?
     b = @board.board_matrix
-    #ROWS
-    if b[0][0] == b[0][1] &&  b[0][1] == b[0][2]
+    # ROWS
+    if b[0][0] == b[0][1] && b[0][1] == b[0][2]
       @is_game_over = true
     elsif b[1][0] == b[1][1] &&  b[1][1] == b[1][2]
       @is_game_over = true
     elsif b[2][0] == b[2][1] &&  b[2][1] == b[2][2]
       @is_game_over = true
-    #COLUMNS
+    # COLUMNS
     elsif b[0][0] == b[1][0] &&  b[1][0] == b[2][0]
       @is_game_over = true
     elsif b[0][1] == b[1][1] &&  b[1][1] == b[2][1]
       @is_game_over = true
-    elsif b[2][0] == b[2][1] &&  b[2][1] == b[2][2]
+    elsif b[0][2] == b[1][2] &&  b[1][2] == b[2][2]
       @is_game_over = true
-    #DIAGONALS
+    # DIAGONALS
     elsif b[0][0] == b[1][1] &&  b[1][1] == b[2][2]
       @is_game_over = true
     elsif b[2][0] == b[1][1] &&  b[1][1] == b[0][2]
       @is_game_over = true
-    elsif @@count_turns==9
+    elsif @count_turns == 9
       @is_game_over = true
     end
   end
@@ -56,7 +54,7 @@ class Game
   def end_game
     @board.draw_board
     puts 'GAME OVER!'
-    if @@count_turns==9
+    if @count_turns == 9
       puts "IT'S A DRAW!"
     else
       puts "#{@controller.player_to_go.name} is the winner!!"
@@ -76,22 +74,22 @@ class Controller
 
   def create_players(players)
     players = Array.new(players)
-    players.each_index { | i | players[i] = Player.new("Player #{i+1}") }
+    players.each_index { |i| players[i] = Player.new("Player #{i + 1}") }
   end
 
   def get_input
     switch_player_turn
-      puts "It is #{@player_to_go.name}'s turn to go!\nEnter which square you want to fill!\n"
-      letter = test_input
-      return [" #{letter} ", @player_to_go.name]
+    puts "It is #{@player_to_go.name}'s turn to go!\nEnter which square you want to fill!\n"
+    letter = test_input
+    [" #{letter} ", @player_to_go.name]
   end
 
   def switch_player_turn
-    if @player_to_go == @players[0]
-      @player_to_go = @players[1]
-    else
-      @player_to_go = @players[0]
-    end
+    @player_to_go = if @player_to_go == @players[0]
+                      @players[1]
+                    else
+                      @players[0]
+                    end
   end
 
   def test_input
@@ -99,12 +97,12 @@ class Controller
 
     while test_passed == false
       letter = gets.chomp.upcase
-      if letter >= "A" and letter <= "I" and !@stored_guesses.include?(letter)
+      if (letter >= 'A') && (letter <= 'I') && !@stored_guesses.include?(letter)
         test_passed = true
         @stored_guesses.push(letter)
       end
     end
-    return letter
+    letter
   end
 end
 
@@ -127,7 +125,7 @@ class Board
 
     print "\n"
     @board_matrix.each do |array|
-      array.each do | element |
+      array.each do |element|
         print vertical_line % @edge_size != 0 ? "#{element}|" : element
         vertical_line += 1
       end
@@ -138,13 +136,13 @@ class Board
 
   def update_board(grid_player_name_array)
     letter = grid_player_name_array[0]
-    symbol = grid_player_name_array[1] === "Player 1" ? " X " : " O "
+    symbol = grid_player_name_array[1] == 'Player 1' ? ' X ' : ' O '
     x = 0
     y = 0
 
     # Translates letter input into coordinate
-    @board_matrix.each_index do | i |
-      @board_matrix.each_index do | j |
+    @board_matrix.each_index do |i|
+      @board_matrix.each_index do |j|
         if @board_matrix[i][j] == letter
           x = i
           y = j
@@ -160,14 +158,13 @@ class Board
   # Creates initial board with letters to show users for input
   def new_board_grid
     unicode = 65
-    @board_matrix.each do | array |
-      @board_matrix.each_with_index do | element, i |
+    @board_matrix.each do |array|
+      @board_matrix.each_with_index do |_element, i|
         array[i] = " #{unicode.chr} "
         unicode += 1
       end
     end
   end
-
 
   def create_bottom_string
     bottom_line_length = @edge_size * 3 + (@edge_size - 1)
@@ -182,13 +179,13 @@ class Board
 
   # Turns numbers into symbols
   def translate_matrix
-    @board_matrix.each do | array |
-      array.each_with_index do | element, i |
+    @board_matrix.each do |array|
+      array.each_with_index do |element, i|
         case element
         when 0
-          array[i] = " O "
+          array[i] = ' O '
         when 1
-          array[i] = " X "
+          array[i] = ' X '
         end
       end
     end
@@ -199,11 +196,13 @@ end
 class Player
   attr_reader :name
   attr_accessor :turn_to_go
+
   def initialize(name)
     @name = name
-    @turn_to_go = false;
+    @turn_to_go = false
   end
 end
 
+# Creates new game with 2 players and a grid with 3 squares per side
 game = Game.new(2, 3)
 game.start_game
